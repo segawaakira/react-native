8ce6bcb7cdc1268e6263a82ca0e618dc6d132419
segawa <>
2019-11-06 01:11:45 +0900

    MemoListScreenに集約

---
+++ memoapp/src/screens/MemoListScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MemoList from '../components/MemoList';
+import CircleButton from '../elements/CircleButton';
+
+class MemoListScreen extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <MemoList />
+        <CircleButton>+</CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoAddButton: {
+    position:'absolute',
+    bottom:32,
+    right:32,
+    width:48,
+    height:48,
+    backgroundColor:'#e31676',
+    borderRadius:24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset:{width:0,height:2},
+    shadowOpacity:0.5,
+    shadowRadius:3,
+  },
+  memoAddButtonTitle: {
+    fontSize:24,
+    lineHeight:24,
+    color:'#fff',
+  },
+});
+
+export default MemoListScreen;
