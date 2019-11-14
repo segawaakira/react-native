2f45756733f5ef78f477ed3f9bb92895abbf14ab
segawa <>
2019-11-14 17:41:23 +0900

    firebaseインストール。環境変数は、env.jsonに。

--- memoapp/App.js
+++ memoapp/App.js
@@ -1,11 +1,23 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator, HeaderStyleInterpolator } from 'react-navigation-stack';
+import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import ENV from './env.json';
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DB_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STRAGE,
+  messagingSenderId: ENV.FIREBASE_SENDER_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 const App = createStackNavigator({
   Login: {
--- memoapp/src/screens/LoginScreen.js
+++ memoapp/src/screens/LoginScreen.js
@@ -2,15 +2,41 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 
 class LoginScreen extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  handleSubmit() {
+    console.log("sumited");
+    // this.props.navigation.navigate('Home)
+
+    // Login処理
+  }
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
           ログイン
         </Text>
-        <TextInput style={styles.input} value="Email Address" />
-        <TextInput style={styles.input} value="password" />
-        <Button title="ログインする" onPress={() => { this.props.navigation.navigate('Home'); }} />
+        <TextInput
+          style={styles.input}
+          value={ this.state.email}
+          onChangeText={(text) => {this.setState({email: text});} }
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Email Address"
+        />
+        <TextInput
+          style={styles.input}
+          value={ this.state.password}
+          onChangeText={(text) => {this.setState({password: text});} }
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <Button title="ログインする" onPress={this.handleSubmit.bind(this)} />
       </View>
     );
   }
